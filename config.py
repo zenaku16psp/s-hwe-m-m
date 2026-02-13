d8c03dd4d86507b9296b88e8fa52ed7d451b8e53
zenaku16psp <pyaesone5p.s.p@gmail.com>
2026-02-13 22:51:29 +0630

    Update config.py

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/zenaku16psp/shwe-narsan",
+    "https://github.com/zenaku16psp/s-hwe-m-m",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
